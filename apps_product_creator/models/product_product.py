d95efe5c014bd456ed18629653c8726c2bfc81f7
Alex Comba <alex.comba@agilebg.com>
2017-10-03 14:02:25 +0200

    [IMP] Replace openerp imports to odoo

--- apps_product_creator/models/product_product.py
+++ apps_product_creator/models/product_product.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class ProductProduct(models.Model):
