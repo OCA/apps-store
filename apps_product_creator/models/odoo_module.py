aa47bc7e564f014c06b95963a6dc6e63a68ef61a
François Honoré <francois.honore@acsone.eu>
2017-10-03 16:10:41 +0200

    Improvements: Edit readme + improvement about code

--- apps_product_creator/models/odoo_module.py
+++ apps_product_creator/models/odoo_module.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Onestein (<http://www.onestein.eu>)
-# Copyright 2017 Alex Comba - Agile Business Group
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from odoo import models, fields, api
+# Copyright (C) 2016-Today: Odoo Community Association (OCA)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
 
 
 class OdooModule(models.Model):
     _inherit = 'odoo.module'
 
     product_template_id = fields.Many2one(
-        'product.template', string="Product Template")
+        'product.template',
+        "Product Template",
+    )
 
     @api.multi
     def action_create_product(self):
@@ -18,42 +19,56 @@ class OdooModule(models.Model):
 
     @api.multi
     def _create_product(self):
-        for module in self:
-            if module.product_template_id:
-                continue
-            matching_template = self.env['product.template'].search(
-                [('odoo_module_id', '=', module.id)], limit=1)
-            if matching_template:
-                module.product_template_id = matching_template
-                continue
-            template_dict = module._get_template_values()
-            new_template = self.env['product.template'].create(template_dict)
-            module.product_template_id = new_template
+        """
+        Create the product template related to the module in current recordset.
+        :return: product.template recordset
+        """
+        product_obj = self.env['product.template']
+        products = self.env['product.template']
+        modules = self.filtered(lambda m: not m.product_template_id)
+        domain = [
+            ('odoo_module_id', '=', modules.ids),
+        ]
+        matching_products = product_obj.search(domain)
+        for odoo_module in modules:
+            product = matching_products.filtered(
+                lambda p: p.odoo_module_id == odoo_module)
+            if not product:
+                product_values = odoo_module._get_template_values()
+                new_product = product_obj.create(product_values)
+                odoo_module.write({
+                    'product_template_id': new_product.id,
+                })
+                products |= new_product
+        return products
 
     @api.multi
     def _get_template_values(self):
+        """
+        Create the dict to create a product.template recordset based on the
+        current recordset.
+        The values dict contains info to link the future product with the
+        current module. It also fill the name of the future product with the
+        name of the current module.
+        :return: dict
+        """
         self.ensure_one()
-
+        attribute_obj = self.env['product.attribute.value']
         milestones = self.module_version_ids.mapped(
-            'repository_branch_id').mapped(
-                'organization_milestone_id').mapped('name')
-        value_ids = self.env['product.attribute.value'].search(
-            [('name', 'in', milestones)]).ids
-
-        res = {
+            'repository_branch_id.organization_milestone_id.name')
+        attributes = attribute_obj.search([('name', 'in', milestones)])
+        attribute = self.env.ref(
+            'github_product_creator.attribute_odoo_version')
+        attribute_line_values = {
+            'attribute_id': attribute.id,
+            'value_ids': [(6, 0, attributes.ids)],
+        }
+        values = {
             'odoo_module_id': self.id,
             'type': 'service',
             'name': self.name,
-            'attribute_line_ids': [(0, 0, {
-                'attribute_id': self.env.ref(
-                    'github_product_creator.attribute_odoo_version').id,
-                'value_ids': [(6, 0, value_ids)],
-            })]
+            'attribute_line_ids': [
+                (0, 0, attribute_line_values),
+            ]
         }
-        return res
-
-    @api.model
-    def cron_create_product(self):
-        modules = self.search(['product_template_id', '=', False])
-        modules.action_create_product()
-        return True
+        return values
