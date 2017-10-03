3f73eefc6a855a64943c1fa01616a11c631ec4f2
Alex Comba <alex.comba@agilebg.com>
2017-10-03 13:56:03 +0200

    Rename module to apps_product_creator

---
+++ apps_product_creator/models/odoo_module.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class OdooModule(models.Model):
+    _inherit = 'odoo.module'
+
+    product_template_id = fields.Many2one(
+        'product.template', string="Product Template")
+
+    @api.multi
+    def action_create_product(self):
+        self._create_product()
+
+    @api.multi
+    def _create_product(self):
+        for module in self:
+            if module.product_template_id:
+                continue
+            matching_template = self.env['product.template'].search(
+                [('odoo_module_id', '=', module.id)], limit=1)
+            if matching_template:
+                module.product_template_id = matching_template
+                continue
+            template_dict = module._get_template_values()
+            new_template = self.env['product.template'].create(template_dict)
+            module.product_template_id = new_template
+
+    @api.multi
+    def _get_template_values(self):
+        self.ensure_one()
+
+        milestones = self.module_version_ids.mapped(
+            'repository_branch_id').mapped(
+                'organization_milestone_id').mapped('name')
+        value_ids = self.env['product.attribute.value'].search(
+            [('name', 'in', milestones)]).ids
+
+        res = {
+            'odoo_module_id': self.id,
+            'type': 'service',
+            'name': self.name,
+            'attribute_line_ids': [(0, 0, {
+                'attribute_id': self.env.ref(
+                    'github_product_creator.attribute_odoo_version').id,
+                'value_ids': [(6, 0, value_ids)],
+            })]
+        }
+        return res
---
+++ apps_product_creator/models/product_product.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.model
+    def create(self, values):
+        if values.get('product_tmpl_id') and values.get('attribute_value_ids'):
+            template = self.env['product.template'].browse(
+                values['product_tmpl_id'])
+            attribute = self.env['product.attribute.value'].browse(
+                values['attribute_value_ids'][0][2]).filtered(
+                lambda x: x.attribute_id == self.env.ref(
+                    'github_product_creator.attribute_odoo_version'))
+
+            if attribute:
+                version = template.odoo_module_id.module_version_ids.filtered(
+                    lambda x:
+                    x.organization_milestone_id.name == attribute.name)
+                if version:
+                    values.update({'odoo_module_version_id': version.id})
+        return super(ProductProduct, self).create(values)
+
+    odoo_module_version_id = fields.Many2one(
+        'odoo.module.version', 'Odoo Module')
+
+    license = fields.Char(
+        string='License (Manifest)', readonly=True,
+        related="odoo_module_version_id.license", store=True)
+    summary = fields.Char(
+        string='Summary (Manifest)', readonly=True,
+        related="odoo_module_version_id.summary", store=True)
+    website = fields.Char(
+        string='Website (Manifest)', readonly=True,
+        related="odoo_module_version_id.website", store=True)
+    external_dependencies = fields.Char(
+        string='External Dependencies (Manifest)',
+        readonly=True, related="odoo_module_version_id.external_dependencies",
+        store=True)
+    description_rst = fields.Char(
+        string='RST Description (Manifest)', readonly=True,
+        related="odoo_module_version_id.description_rst", store=True)
+    description_rst_html = fields.Html(
+        string='HTML the RST Description', readonly=True,
+        related="odoo_module_version_id.description_rst_html", store=True)
+    version = fields.Char(
+        string='Version (Manifest)', readonly=True,
+        related="odoo_module_version_id.version", store=True)
+    author = fields.Char(
+        string='Author (Manifest)', readonly=True,
+        related="odoo_module_version_id.author", store=True)
+    image = fields.Binary(
+        string='Icon Image', reaonly=True,
+        related="odoo_module_version_id.image", store=True)
+    github_url = fields.Char(
+        string='Github URL', readonly=True,
+        related="odoo_module_version_id.github_url", store=True)
