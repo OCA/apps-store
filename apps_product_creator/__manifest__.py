7703b17d9bdbac40c50f3c2d4b5b2c34f459ffa9
antonio <espo-tony@hotmail.it>
2017-10-16 09:54:05 +0200

    [FIX] Adapted to recent changes in dependency module github_connector

--- apps_product_creator/__manifest__.py
+++ apps_product_creator/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016-Today: Odoo Community Association (OCA)
+# Copyright (C) 2017-Today: Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Github product creator',
