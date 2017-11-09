2d7a251dbb68ca205b907a7a9faaf46eaa215ca4
antonio <espo-tony@hotmail.it>
2017-11-09 15:46:26 +0100

    Implemented Unit tests

---
+++ apps_product_creator/tests/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017-Today: Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import test_apps_product_creator
