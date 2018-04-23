5d99b4248fa9ab9d2681b3cd661f25b7cdc18026
Divya Modi <divya.modi@bizzappdev.com>
2018-04-23 19:34:11 +0530

    [IMP]add README.rst file

--- website_apps_store/__manifest__.py
+++ website_apps_store/__manifest__.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2017-Today: Odoo Community Association (OCA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2017 BizzAppDev
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Website Apps Store',
-    'summary': 'Website AppStore',
-    'version': '10.0.1.0.0',
-    'website': 'http://www.bizzappdev.com',
-    'category': 'Connector',
-    'license': 'AGPL-3',
-    'author': 'Bizzappdev',
-    'depends': [
+    "name": "Website Apps Store",
+    "version": "10.0.1.0.0",
+    "author": "Bizzappdev",
+    "website": "https://github.com/OCA/apps-store",
+    "license": "AGPL-3",
+    "category": "Sales",
+    "depends": [
         'website_sale',
         'apps_download',
         'apps_product_creator',
     ],
-    'data': [
+    "summary": "Website Apps Store",
+    "data": [
         'views/assets.xml',
         'views/templates.xml',
     ],
-    'demo': [
+    "demo": [
     ],
     'installable': True,
     'auto_install': False,
--- website_apps_store/models/product.py
+++ website_apps_store/models/product.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2017-Today: Odoo Community Association (OCA)
+# Copyright 2017 BizzAppDev
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
