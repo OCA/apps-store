f29df95709b49f595a8706e75d47fcedc8e3e591
Divya Modi <divya.modi@bizzappdev.com>
2018-06-06 18:01:50 +0530

    [IMP]Added test cases for new features

---
+++ website_apps_store/static/src/js/website_apps_store_tour.js
@@ -0,0 +1,104 @@
+odoo.define('website_apps_store.tour_custom', function (require) {
+'use strict';
+
+var Tour = require("web_tour.tour");
+var base = require("web_editor.base");
+
+    Tour.register('download_zip', {
+        name: "Download Product Zip",
+        url: '/shop',
+        test: true,
+        wait_for: base.ready()
+        },[
+            {
+                content: "Shop",
+                trigger: ".oe_product_image img:first"
+            },
+            {
+                content: "Select Version",
+                trigger: "input[type=radio]",
+                run: function(){
+                    $('input[type=radio]:last').attr("checked", "checked");
+                }
+            },
+
+            {
+                content: "Download",
+                trigger: "button:contains(Download)"
+            }
+        ]
+    );
+
+    Tour.register('select_version_search', {
+        name: "Select Version",
+        url: '/shop',
+        test: true,
+        wait_for: base.ready()
+        },[
+            {
+                content: "Shop",
+                trigger: ".dropdown_version_by a.dropdown-toggle"
+            },
+            {
+                content: "Select Version",
+                trigger: ".dropdown_version_by .dropdown-menu a:first",
+            },
+        ]
+    );
+
+    Tour.register('select_author_search', {
+        name: "Select Author",
+        url: '/shop',
+        test: true,
+        wait_for: base.ready()
+        },[
+            {
+                content: "Shop",
+                trigger: ".dropdown_author_by a.dropdown-toggle"
+            },
+            {
+                content: "Select Author",
+                trigger: ".dropdown_author_by .dropdown-menu a:first",
+            },
+        ]
+    );
+
+    Tour.register('select_category_search', {
+        name: "Select Category",
+        url: '/shop',
+        test: true,
+        wait_for: base.ready()
+        },[
+            {
+                content: "Shop",
+                trigger: ".dropdown_category_by a.dropdown-toggle"
+            },
+            {
+                content: "Select Category",
+                trigger: ".dropdown_category_by .dropdown-menu a:first",
+            },
+        ]
+    );
+
+    Tour.register('module_search', {
+        name: "Select Product(Module)",
+        url: '/shop',
+        test: true,
+        wait_for: base.ready()
+        },[
+            {
+                content: "Shop",
+                trigger: ".search-query",
+                run: function(){
+                    $('.search-query').attr('value', 'account');
+                }
+
+            },
+            {
+                content: "Search Button",
+                trigger: ".oe_search_button",
+            },
+        ]
+    );
+
+});
