17b678a259887c5c55fe97db354c3a57a3b6f6cf
Divya Modi <divya.modi@bizzappdev.com>
2018-08-31 19:26:08 +0530

    [WIP]partial commit for migration of apps-store

---
+++ website_apps_store/tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2017-Today: Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import test_website_apps_store
+from . import test_tour_website_apps_store
