94bfc003d2f216495dc7c6f17f7f520096b58fea
Divya Modi <divya.modi@bizzappdev.com>
2018-09-20 19:26:02 +0530

    [FIX] made changes as per suggested

--- website_apps_store/tests/test_tour_website_apps_store.py
+++ website_apps_store/tests/test_tour_website_apps_store.py
@@ -1,6 +1,11 @@
 # Copyright 2017-2018 BizzAppDev
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import tests
+import mock
+
+mock_get_module_path = (
+    'odoo.addons.apps_download.models.product.ProductProduct._get_module_path'
+)
 
 
 @tests.common.at_install(False)
@@ -10,6 +15,17 @@ class TestUi(tests.HttpCase):
     def setUp(self):
         super(TestUi, self).setUp()
         self.tour = "odoo.__DEBUG__.services['web_tour.tour']"
+
+    def test_download_zip_tour(self):
+        test_path = 'odoo.addons.apps_download.tests.test_modules.test_module'
+        with mock.patch(mock_get_module_path) as mock_func:
+            mock_func.return_value = test_path
+            self.phantom_js(
+                "/shop",
+                self.tour + ".run('download_zip')",
+                self.tour + ".tours.download_zip.ready",
+                login="admin"
+            )
 
     def test_select_version_search_tour(self):
         self.phantom_js(
