3833f3607bbda9c55543e1d1c433a1b35c222556
Ruchir Shukla <ruchir@bizzappdev.com>
2018-10-15 21:05:17 +0530

    [IMP] while clean the odoo module version archive the product

--- apps_product_creator/models/__init__.py
+++ apps_product_creator/models/__init__.py
@@ -1,3 +1,4 @@
 from . import product_template
 from . import product_product
 from . import odoo_module
+from . import odoo_module_veresion
---
+++ apps_product_creator/models/odoo_module_veresion.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2017-Today: Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models, api
+
+
+class OdooModuleVersion(models.Model):
+    _inherit = 'odoo.module.version'
+
+    @api.multi
+    def _process_clean_module_version(self):
+        for module_version in self:
+            product = self.env['product.product'].search(
+                [('odoo_module_version_id', '=', module_version.id)])
+            product.write({'active': False})
+            all_varient_archived = all(
+                [
+                    not a for a in
+                    product.product_tmpl_id.product_variant_ids.mapped(
+                        'active')
+                ]
+            )
+            if all_varient_archived:
+                product.product_tmpl_id.write(
+                    {'active': False, 'website_published': False})
+        return super(OdooModuleVersion, self)._process_clean_module_version()
