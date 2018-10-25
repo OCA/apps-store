95fa60c1f0922ebe704f9d479a972e1c0bd488ce
Ruchir Shukla <ruchir@bizzappdev.com>
2018-10-25 11:29:15 +0530

    [IMP]improve logic with less iterations

--- apps_product_creator/models/odoo_module_version.py
+++ apps_product_creator/models/odoo_module_version.py
@@ -8,18 +8,18 @@ class OdooModuleVersion(models.Model):
 
     @api.multi
     def _process_clean_module_version(self):
+        product_obj = self.env['product.product']
         for module_version in self:
-            product = self.env['product.product'].search(
+            product = product_obj.search(
                 [('odoo_module_version_id', '=', module_version.id)])
             product.write({'active': False})
-            all_variant_archived = all(
+            active_variants = product_obj.search_count(
                 [
-                    not a for a in
-                    product.product_tmpl_id.product_variant_ids.mapped(
-                        'active')
+                    ('product_tmpl_id', '=', product.product_tmpl_id.id),
+                    ('active', '=', True)
                 ]
             )
-            if all_variant_archived:
+            if not active_variants:
                 product.product_tmpl_id.write(
                     {'active': False, 'website_published': False})
         return super(OdooModuleVersion, self)._process_clean_module_version()
