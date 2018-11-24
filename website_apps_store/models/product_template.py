3ffdb6e5d0e67d8a0603968ecdd8765474591648
Ruchir Shukla <ruchir@bizzappdev.com>
2018-11-24 12:54:11 +0530

    [IMP]added download count fields and increament it when donwloaded

--- website_apps_store/models/product_template.py
+++ website_apps_store/models/product_template.py
@@ -1,11 +1,21 @@
 # Copyright (C) 2017-Today: Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import models, fields, api
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+
+    @api.multi
+    @api.depends('product_variant_ids', 'product_variant_ids.download_count')
+    def _compute_total_download_count(self):
+        for product in self:
+            product.total_download_count = sum(
+                product.mapped('product_variant_ids.download_count'))
+
+    total_download_count = fields.Integer(
+        compute="_compute_total_download_count", store=True)
 
     def get_author_details(self):
         author_ids = []
