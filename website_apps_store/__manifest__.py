80a93086f4f7295c06c023a563d5630bba589a78
Shruti Singh <shruti.singh@bizzappdev.com>
2018-12-25 19:34:21 +0530

    [FIX]website_apps_store : add demo/product_template_demo.xml in menifest.py

--- website_apps_store/__manifest__.py
+++ website_apps_store/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Website Apps Store",
-    "version": "11.0.2.2.0",
+    "version": "11.0.2.2.1",
     'author': 'Odoo Community Association (OCA), BizzAppDev',
     "website": "https://github.com/OCA/apps-store",
     "license": "AGPL-3",
@@ -16,6 +16,9 @@
     "data": [
         'views/assets.xml',
         'views/templates.xml',
+    ],
+    'demo': [
+        'demo/product_template_demo.xml',
     ],
     'installable': True,
     'auto_install': False,
