53d70a67ae31a11bca541caf7c4f40954cb69864
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-08-05 18:52:01 +0200

    [IMP] apps_download: black, isort, prettier

--- apps_download/tests/test_app_download.py
+++ apps_download/tests/test_app_download.py
@@ -2,9 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests.common import TransactionCase
-from odoo.exceptions import ValidationError
 import os
+
+from odoo.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
 
 
 class TestAppDownload(TransactionCase):
@@ -13,46 +14,37 @@ class TestAppDownload(TransactionCase):
     def setUp(self):
         super(TestAppDownload, self).setUp()
         test_path = os.path.dirname(os.path.realpath(__file__))
-        module_path = os.path.join(
-            test_path, 'test_modules', 'broken_lint')
-        self.sub_child_product = self.env['product.product'].create(
+        module_path = os.path.join(test_path, "test_modules", "broken_lint")
+        self.sub_child_product = self.env["product.product"].create(
+            {"name": "business_requirement", "module_path": module_path}
+        )
+        module_path = os.path.join(test_path, "test_modules", "second_module")
+        self.child_product = self.env["product.product"].create(
             {
-                'name': 'business_requirement',
-                'module_path': module_path
+                "name": "business_requirement_deliverable",
+                "dependent_product_ids": [(6, 0, self.sub_child_product.ids)],
+                "module_path": module_path,
             }
         )
-        module_path = os.path.join(
-            test_path, 'test_modules', 'second_module')
-        self.child_product = self.env['product.product'].create(
+        module_path = os.path.join(test_path, "test_modules", "test_module")
+        self.main_app_product = self.env["product.product"].create(
             {
-                'name': 'business_requirement_deliverable',
-                'dependent_product_ids': [(6, 0, self.sub_child_product.ids)],
-                'module_path': module_path
-            }
-        )
-        module_path = os.path.join(
-            test_path, 'test_modules', 'test_module')
-        self.main_app_product = self.env['product.product'].create(
-            {
-                'name': 'business_requirement_deliverable_project',
-                'module_path': module_path
+                "name": "business_requirement_deliverable_project",
+                "module_path": module_path,
             }
         )
 
     def test_dependency(self):
         self.main_app_product.write(
-            {
-                'dependent_product_ids': [(6, 0, self.child_product.ids)]
-            }
+            {"dependent_product_ids": [(6, 0, self.child_product.ids)]}
         )
         self.assertEqual(
-            len(self.main_app_product.dependent_product_ids.ids), 1,
-            'Dependency: App Dependency does not set properlly')
+            len(self.main_app_product.dependent_product_ids.ids),
+            1,
+            "Dependency: App Dependency does not set properlly",
+        )
         self.main_app_product.generate_zip_file()
         with self.assertRaises(ValidationError):
             self.main_app_product.write(
-                {
-                    'dependent_product_ids': [
-                        (6, 0, self.main_app_product.ids)]
-                }
+                {"dependent_product_ids": [(6, 0, self.main_app_product.ids)]}
             )
