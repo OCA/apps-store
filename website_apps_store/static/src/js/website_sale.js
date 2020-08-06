62007e5001b5a788cdeea6984d7298056a98eaa4
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2020-08-06 12:12:13 +0200

    [IMP] website_apps_store: black, isort, prettier

--- website_apps_store/static/src/js/website_sale.js
+++ website_apps_store/static/src/js/website_sale.js
@@ -1,42 +1,43 @@
 /* global grecaptcha */
 /**
-*    Copyright 2018 BizzAppDev
-*    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-**/
+ *    Copyright 2018 BizzAppDev
+ *    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+ **/
 
-odoo.define('website_apps_store.website_sale', function (require) {
-    'use strict';
+odoo.define("website_apps_store.website_sale", function(require) {
+    "use strict";
 
-    require('web.dom_ready');
-    var ajax = require('web.ajax');
-    var sAnimations = require('website.content.snippets.animation');
+    require("web.dom_ready");
+    var ajax = require("web.ajax");
+    var sAnimations = require("website.content.snippets.animation");
     var WebsiteSale = sAnimations.registry.WebsiteSale;
 
     WebsiteSale.include({
         read_events: _.extend({}, WebsiteSale.prototype.read_events, {
-            'click #download_zip': '_onClickDownload',
+            "click #download_zip": "_onClickDownload",
         }),
 
-        init: function () {
+        init: function() {
             this._super.apply(this, arguments);
-            var $captchas = $('.o_website_form_recaptcha');
-            ajax.post('/website/recaptcha/', {}).then(
-                function (result) {
-                    var data = JSON.parse(result);
-                    $captchas.append($(
+            var $captchas = $(".o_website_form_recaptcha");
+            ajax.post("/website/recaptcha/", {}).then(function(result) {
+                var data = JSON.parse(result);
+                $captchas.append(
+                    $(
                         '<div class="g-recaptcha" data-sitekey="' +
-                        data.site_key + '"></div>'
-                    ));
-                    if ($captchas.length) {
-                        $.getScript('https://www.google.com/recaptcha/api.js');
-                    }
+                            data.site_key +
+                            '"></div>'
+                    )
+                );
+                if ($captchas.length) {
+                    $.getScript("https://www.google.com/recaptcha/api.js");
                 }
-            );
+            });
         },
 
-        _onChangeCombination: function (ev, $parent, combination) {
+        _onChangeCombination: function(ev, $parent, combination) {
             this._super.apply(this, arguments);
-            if ('is_odoo_module' in combination && combination.is_odoo_module) {
+            if ("is_odoo_module" in combination && combination.is_odoo_module) {
                 var $tech_deatil = this.$(".tech_deatil");
                 var $license_detail = this.$(".license_detail");
                 var $license_url = this.$(".license_url");
@@ -49,7 +50,7 @@ odoo.define('website_apps_store.website_sale', function (require) {
 
                 $tech_deatil.text(combination.technical_name);
                 $license_detail.text(combination.license);
-                $license_url.attr('href', combination.license_url);
+                $license_url.attr("href", combination.license_url);
                 $author_detail.text(combination.author);
                 $website_detail.text(combination.website);
                 $repo_detail.text(combination.repository);
@@ -60,26 +61,33 @@ odoo.define('website_apps_store.website_sale', function (require) {
             }
         },
 
-        _onClickDownload: function (ev) {
-            var $product_template_id = this.$('.product_template_id');
+        _onClickDownload: function(ev) {
+            var $product_template_id = this.$(".product_template_id");
             var product_template_id = $product_template_id.val();
-            var $product_id = this.$('.product_id');
+            var $product_id = this.$(".product_id");
             var product_id = $product_id.val();
             ev.preventDefault();
-            var google_captcha = $('#g-recaptcha-response').val();
+            var google_captcha = $("#g-recaptcha-response").val();
             if (!google_captcha) {
                 return;
             }
-            if (typeof grecaptcha !== 'undefined') {
+            if (typeof grecaptcha !== "undefined") {
                 grecaptcha.reset();
             }
             if (product_id) {
-                window.location.href = "/shop/download_product_zip/" +
-                    product_template_id + '/' + product_id + '/' +
+                window.location.href =
+                    "/shop/download_product_zip/" +
+                    product_template_id +
+                    "/" +
+                    product_id +
+                    "/" +
                     google_captcha;
             } else {
-                window.location.href = "/shop/download_product_zip/" +
-                    product_template_id + '/' + google_captcha;
+                window.location.href =
+                    "/shop/download_product_zip/" +
+                    product_template_id +
+                    "/" +
+                    google_captcha;
             }
         },
     });
