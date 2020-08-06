cb743d30c3ad55e2e42098edef86dbf5811b54b9
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2020-08-06 12:15:33 +0200

    [MIG] website_apps_store: Migration to 13.0

--- website_apps_store/static/src/js/website_sale.js
+++ website_apps_store/static/src/js/website_sale.js
@@ -8,9 +8,9 @@ odoo.define("website_apps_store.website_sale", function(require) {
     "use strict";
 
     require("web.dom_ready");
-    var ajax = require("web.ajax");
-    var sAnimations = require("website.content.snippets.animation");
-    var WebsiteSale = sAnimations.registry.WebsiteSale;
+    const ajax = require("web.ajax");
+    const sAnimations = require("website.content.snippets.animation");
+    const WebsiteSale = sAnimations.registry.WebsiteSale;
 
     WebsiteSale.include({
         read_events: _.extend({}, WebsiteSale.prototype.read_events, {
@@ -19,15 +19,11 @@ odoo.define("website_apps_store.website_sale", function(require) {
 
         init: function() {
             this._super.apply(this, arguments);
-            var $captchas = $(".o_website_form_recaptcha");
-            ajax.post("/website/recaptcha/", {}).then(function(result) {
-                var data = JSON.parse(result);
+            const $captchas = $(".o_website_form_recaptcha");
+            ajax.post("/website/recaptcha/", {}).then(result => {
+                const data = JSON.parse(result);
                 $captchas.append(
-                    $(
-                        '<div class="g-recaptcha" data-sitekey="' +
-                            data.site_key +
-                            '"></div>'
-                    )
+                    $(`<div class="g-recaptcha" data-sitekey="${data.site_key}"></div>`)
                 );
                 if ($captchas.length) {
                     $.getScript("https://www.google.com/recaptcha/api.js");
@@ -38,15 +34,15 @@ odoo.define("website_apps_store.website_sale", function(require) {
         _onChangeCombination: function(ev, $parent, combination) {
             this._super.apply(this, arguments);
             if ("is_odoo_module" in combination && combination.is_odoo_module) {
-                var $tech_deatil = this.$(".tech_deatil");
-                var $license_detail = this.$(".license_detail");
-                var $license_url = this.$(".license_url");
-                var $author_detail = this.$(".author_detail");
-                var $website_detail = this.$(".website_detail");
-                var $repo_detail = this.$(".repo_detail");
-                var $rst_html = this.$(".desc_rst");
-                var $app_summary = this.$(".app_summary");
-                var $name_product = this.$(".name_product");
+                const $tech_deatil = this.$(".tech_deatil");
+                const $license_detail = this.$(".license_detail");
+                const $license_url = this.$(".license_url");
+                const $author_detail = this.$(".author_detail");
+                const $website_detail = this.$(".website_detail");
+                const $repo_detail = this.$(".repo_detail");
+                const $rst_html = this.$(".desc_rst");
+                const $app_summary = this.$(".app_summary");
+                const $name_product = this.$(".name_product");
 
                 $tech_deatil.text(combination.technical_name);
                 $license_detail.text(combination.license);
@@ -54,7 +50,9 @@ odoo.define("website_apps_store.website_sale", function(require) {
                 $author_detail.text(combination.author);
                 $website_detail.text(combination.website);
                 $repo_detail.text(combination.repository);
-                $repo_detail.parent()[0].href = combination.repository;
+                if ($repo_detail.length) {
+                    $repo_detail.parent()[0].href = combination.repository;
+                }
                 $rst_html.html(combination.rst_html);
                 $app_summary.text(combination.app_summary);
                 $name_product.text(combination.name_product);
@@ -62,18 +60,18 @@ odoo.define("website_apps_store.website_sale", function(require) {
         },
 
         _onClickDownload: function(ev) {
-            var $product_template_id = this.$(".product_template_id");
-            var product_template_id = $product_template_id.val();
-            var $product_id = this.$(".product_id");
-            var product_id = $product_id.val();
             ev.preventDefault();
-            var google_captcha = $("#g-recaptcha-response").val();
+            const google_captcha = $("#g-recaptcha-response").val();
             if (!google_captcha) {
                 return;
             }
             if (typeof grecaptcha !== "undefined") {
                 grecaptcha.reset();
             }
+            const $product_template_id = this.$(".product_template_id");
+            const product_template_id = $product_template_id.val();
+            const $product_id = this.$(".product_id");
+            const product_id = $product_id.val();
             if (product_id) {
                 window.location.href =
                     "/shop/download_product_zip/" +
