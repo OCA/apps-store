655c6d34fa7f1a8f86651380c04e205279a192f0
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2020-08-06 12:12:13 +0200

    [IMP] website_apps_store: black, isort, prettier

--- website_apps_store/tests/test_tour_website_apps_store.py
+++ website_apps_store/tests/test_tour_website_apps_store.py
@@ -1,34 +1,37 @@
 # Copyright 2017-2018 BizzAppDev
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import tests
-import mock
 import os
 
+import mock
+
+from odoo import tests
+
 mock_get_module_path = (
-    'odoo.addons.apps_download.models.product_product.ProductProduct'
-    '._get_module_path'
+    "odoo.addons.apps_download.models.product_product.ProductProduct"
+    "._get_module_path"
 )
 
 
 @tests.common.at_install(False)
 @tests.common.post_install(True)
 class TestUi(tests.HttpCase):
-
     def setUp(self):
         super(TestUi, self).setUp()
         self.tour = "odoo.__DEBUG__.services['web_tour.tour']"
 
     def test_download_zip_tour(self):
         test_path = os.path.dirname(os.path.realpath(__file__))
-        test_path = test_path.split('/website_apps_store')[0]
+        test_path = test_path.split("/website_apps_store")[0]
         test_module_path = os.path.join(
-            test_path + '/apps_download' + '/tests', 'test_modules',
-            'second_module')
+            test_path + "/apps_download" + "/tests", "test_modules", "second_module"
+        )
         with mock.patch(mock_get_module_path) as mock_func:
             mock_func.return_value = test_module_path
             self.browser_js(
-                "/shop/page/2", self.tour + ".run('download_zip')",
-                self.tour + ".tours.download_zip.ready", login="admin"
+                "/shop/page/2",
+                self.tour + ".run('download_zip')",
+                self.tour + ".tours.download_zip.ready",
+                login="admin",
             )
 
     def test_select_version_search_tour(self):
@@ -36,7 +39,7 @@ class TestUi(tests.HttpCase):
             "/shop",
             self.tour + ".run('select_version_search')",
             self.tour + ".tours.select_version_search.ready",
-            login="admin"
+            login="admin",
         )
 
     def test_select_category_search_tour(self):
@@ -44,7 +47,7 @@ class TestUi(tests.HttpCase):
             "/shop",
             self.tour + ".run('select_category_search')",
             self.tour + ".tours.select_category_search.ready",
-            login="admin"
+            login="admin",
         )
 
     def test_module_search_tour(self):
@@ -52,7 +55,7 @@ class TestUi(tests.HttpCase):
             "/shop",
             self.tour + ".run('module_search')",
             self.tour + ".tours.module_search.ready",
-            login="admin"
+            login="admin",
         )
 
     def test_technical_name_search_tour(self):
@@ -60,5 +63,5 @@ class TestUi(tests.HttpCase):
             "/shop",
             self.tour + ".run('technical_name_search')",
             self.tour + ".tours.technical_name_search.ready",
-            login="admin"
+            login="admin",
         )
