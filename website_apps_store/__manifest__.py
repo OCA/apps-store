9cdf0978be04a5ebb5dc5c5791f45b4e091134b6
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-11-18 16:08:13 +0100

    [FIX] website_apps_store: Modify website href on combination change
    Steps to reproduce the problem:
    
    - Have a module like base_location with the initial version with website
      URL A, and a newer version with website URL B
    - Switch the version in the shop product detail.
    - The text of the URL is changed, but not the href.
    
    Fixes https://github.com/OCA/website/issues/858

--- website_apps_store/__manifest__.py
+++ website_apps_store/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Website Apps Store",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "author": "Odoo Community Association (OCA), BizzAppDev",
     "website": "https://github.com/OCA/apps-store",
     "license": "AGPL-3",
